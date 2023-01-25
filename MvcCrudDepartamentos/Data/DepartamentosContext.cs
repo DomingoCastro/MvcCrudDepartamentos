be034ce9f0108cc1bd20c36dd9216c097840ea41
DomingoCastro <domingocastro1998@outlook.com>
2023-01-25 18:49:42 +0100

    Add project files.

---
+++ MvcCrudDepartamentos/Data/DepartamentosContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCrudDepartamentos.Models;
+
+namespace MvcCrudDepartamentos.Data
+{
+    public class DepartamentosContext: DbContext
+    {
+        public DepartamentosContext
+            (DbContextOptions<DepartamentosContext> options): base(options) { }
+        public List<Departamento> Departamentos { get; set; }
+    }
+}
